9cc174255be3e55d53232922c2888df633146eb5
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-04-07 18:04:57 +0300

    first commit

---
+++ smartgrid.js
@@ -0,0 +1,35 @@
+module.exports = {
+	columns: 12,
+	offset: "30px",
+	container: {
+		maxWidth: "1170px",
+		fields: "30px"
+	},
+	breakPoints: {
+		md: {
+			width: "900px"
+		},
+		sm: {
+			width: "720px",
+			fields: "24px"
+		},
+		xs: {
+			width: "576px",
+			/*
+			offset: "5px",
+			fields: "5px"
+			*/
+		},
+		xxs: {
+			width: "290px",
+			fields: "15px"
+		}
+	},
+	mobileFirst: false,
+	tab: '\t',
+	/*
+	mixinNames: {
+		  container: 'container'
+	},
+	detailedCalc: true*/
+}
